23f5405173c6b8933ca9fd31c47db07390825556
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-29 21:43:25 +0300

    Complete 1 task

--- constructors-and-destructors/constructors-and-destructors.cpp
+++ constructors-and-destructors/constructors-and-destructors.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include "triangle.h"
+
+using namespace std;
 
 int main()
 {
-   std::cout << "Hello World!\n";
-}
+   auto* empty = new triangle();
+   auto* with_params = new triangle(10, 15, 30);
 
+   empty->set_a(100);
+   empty->set_b(200);
+   empty->set_c(300);
+
+   cout << endl << "Show empty-object (constructor without params)" <<
+      "after setting values : " << endl
+      << empty->to_string() << endl;
+
+   auto* copied = new triangle(*with_params);
+
+   delete empty;
+   delete copied;
+   delete with_params;
+}
--- constructors-and-destructors/triangle.h
+++ constructors-and-destructors/triangle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 #include <iostream>
 #include<cmath>
 #include <string>
@@ -6,23 +7,42 @@
 class triangle
 {
 public:
-   triangle() = default;
+
+   triangle()
+   {
+      std::cout << "Constructor without params:" << std::endl;
+
+      this->a_ = -1;
+      this->b_ = -1;
+      this->c_ = -1;
+
+      std::cout << to_string() << std::endl;
+   }
 
    triangle(const int a, const int b, const int c)
       : a_(a),
       b_(b),
       c_(c)
    {
+      std::cout << "Constructor with params:" << std::endl;
+      std::cout << to_string() << std::endl;
    }
 
    triangle(const triangle& triangle)
    {
+      std::cout << "Copy constructor:" << std::endl;
+
       this->a_ = triangle.get_a();
       this->b_ = triangle.get_b();
       this->c_ = triangle.get_c();
+
+      std::cout << to_string() << std::endl;
    }
 
-   ~triangle() = default;
+   ~triangle()
+   {
+      std::cout << "Call the destructor" << std::endl;
+   }
 
    bool is_exist() const
    {
@@ -34,14 +54,14 @@ public:
 
    int get_perimeter() const
    {
-      return a_ + b_ + c_;
+      return is_exist() ? a_ + b_ + c_ : -1;
    }
 
    double get_area() const
    {
       const int p = get_perimeter();
 
-      return sqrt(p * (p - a_) * (p - b_) * (p - c_));
+      return is_exist() ? sqrt(p * (p - a_) * (p - b_) * (p - c_)) : -1;
    }
 
    int get_a() const
@@ -52,6 +72,7 @@ public:
    void set_a(const int a)
    {
       this->a_ = a;
+      std::cout << "Call the [a] field setter" << std::endl;
    }
 
    int get_b() const
@@ -62,6 +83,7 @@ public:
    void set_b(const int b)
    {
       this->b_ = b;
+      std::cout << "Call the [b] field setter" << std::endl;
    }
 
    int get_c() const
@@ -72,18 +94,19 @@ public:
    void set_c(const int c)
    {
       this->c_ = c;
+      std::cout << "Call the [c] field setter" << std::endl;
    }
 
    std::string to_string() const
    {
-      std::string dump = "triangle: {\n";
-      dump + "side A: " + std::to_string(a_) + ",\n";
-      dump += "side B: " + std::to_string(b_) + ",\n";
-      dump += "side C: " + std::to_string(c_) + ",\n";
-      dump += "is exist: " + std::to_string(is_exist()) + ",\n";
-      dump += "perimeter: " + std::to_string(get_perimeter()) + ",\n";
-      dump += "area: " + std::to_string(get_area()) + ",\n";
-      dump += "}";
+      std::string dump = "triangle: [{\n";
+      dump += "  side A: " + std::to_string(a_) + ",\n";
+      dump += "  side B: " + std::to_string(b_) + ",\n";
+      dump += "  side C: " + std::to_string(c_) + ",\n";
+      dump += "  is exist: " + std::to_string(is_exist()) + ",\n";
+      dump += "  perimeter: " + std::to_string(get_perimeter()) + ",\n";
+      dump += "  area: " + std::to_string(get_area()) + ",\n";
+      dump += "}];\n";
 
       return dump;
    }
